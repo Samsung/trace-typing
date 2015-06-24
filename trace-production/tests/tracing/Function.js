03020e70f98bc408e4529527cd0b92bc8cb4bedc
Esben Andreasen <esbenandreasen@gmail.com>
2015-06-24 13:47:10 +0200

    expanded smoke test

--- trace-production/tests/tracing/Function.js
+++ trace-production/tests/tracing/Function.js
@@ -5,3 +5,21 @@ var h = new Function("x", "return x;")
 var vf = f();
 var vg = g();
 var vh = h();
+
+try {
+    new Function('p1')();
+} catch (e) {
+
+}
+
+try {
+    new Function('p1', 'p2')();
+} catch (e) {
+
+}
+
+try {
+    new Function('p1', 'p2', 'p3')();
+} catch (e) {
+
+}
