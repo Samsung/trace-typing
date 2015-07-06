d682fc167390fc155d6f2692343b23331fa1bcec
Esben Andreasen <esbenandreasen@gmail.com>
2015-07-06 21:20:33 +0200

    xml2js exercise-code

--- trace-production/tests/exercisedApps/xml2js/main.js
+++ trace-production/tests/exercisedApps/xml2js/main.js
@@ -1,1 +1,13 @@
-require('xml2js');
+var xml2js = require('xml2js');
+
+// test 1
+var parseString = xml2js.parseString;
+var xml = "<root>Hello xml2js!</root>"
+parseString(xml, function (err, result) {
+    // console.dir(result);
+});
+
+// test 2
+var obj = {name: "Super", Surname: "Man", age: 23};
+var builder = new xml2js.Builder();
+var xml = builder.buildObject(obj);
