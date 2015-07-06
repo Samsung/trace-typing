d1a36f18195b3ba7ee1ce36c4ed004741d91282e
Esben Andreasen <esbenandreasen@gmail.com>
2015-07-06 21:30:55 +0200

    ejs exercise-code

--- trace-production/tests/exercisedApps/ejs/main.js
+++ trace-production/tests/exercisedApps/ejs/main.js
@@ -1,1 +1,9 @@
-require('ejs');
+var ejs = require('ejs'),
+    users = ['geddy', 'neil', 'alex'];
+
+ejs.compile("<% if (user) { %>" + "<h2><%= user.name %></h2>" + "<% } %>");
+
+ejs.render('<%= users.join(" | "); %>', {users: users});
+
+ejs.render('<?= users.join(" | "); ?>', {users: users}, {delimiter: '?'});
+
