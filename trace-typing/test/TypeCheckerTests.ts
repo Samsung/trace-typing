9eafac38b0bcabb712dc8a276a79af5fd726296f
Esben Andreasen <esbenandreasen@gmail.com>
2015-07-06 21:05:20 +0200

    more benchmarks

--- trace-typing/test/TypeCheckerTests.ts
+++ trace-typing/test/TypeCheckerTests.ts
@@ -170,9 +170,12 @@ describe("TypeChecker unit tests", function () {
     });
 });
 describe("Type check traces and display table", function () {
-    var bigApps = [/*'gulp', */ 'lodash', 'minimist', 'optparse', /*'express', 'grunt', */ 'lazy.js', 'underscore'/*, 'coffee-script'*/, 'escodegen'];
-    //var bigApps = [/*'gulp', */ 'lodash', 'minimist', 'optparse', /*'express', 'grunt', */ 'lazy.js', 'underscore'/*, 'coffee-script'*/, /*'escodegen'*/];
-    describe.only("Type check everything ", function () {
+    var oldBigApps = [/*'gulp', */ 'lodash', 'minimist', 'optparse', /*'express', 'grunt', */ 'lazy.js', 'underscore', 'coffee-script', 'escodegen'];
+    oldBigApps = [];
+    var newBigApps = ['ejs','esprima','qs','typescript','validator','xml2js',];
+
+    var bigApps = oldBigApps.concat(newBigApps);
+    describe("Type check everything ", function () {
         this.timeout(10 * 60 * 1000);
         var traceImporter:TraceImporter.TraceImporter = new TraceImporter.TraceImporter();
         traceImporter.getAllTraceFiles().forEach(function (file) {
@@ -183,19 +186,19 @@ describe("Type check traces and display table", function () {
                 return; // ignore
             }
             var allTypes = [
-                //[inferenceConfigs.simpleSubtypingWithUnion, 'simpleSubtypingWithUnion']
-                //, [inferenceConfigs.simpleSubtyping, 'simpleSubtyping']
-                //, [inferenceConfigs.fullIntersection, 'intersection']
-                , [inferenceConfigs.SJS, 'SJS']
+                [inferenceConfigs.simpleSubtypingWithUnion, 'simpleSubtypingWithUnion']
+                , [inferenceConfigs.simpleSubtyping, 'simpleSubtyping']
+                , [inferenceConfigs.fullIntersection, 'intersection']
+                //, [inferenceConfigs.SJS, 'SJS']
             ];
             var allFunctionTypes = [
                 [TypeLattices.FunctionTypeLatticeKinds.FunctionIntersection, "IntersectionFunctions", false, false],
-                // [TypeLattices.FunctionTypeLatticeKinds.FunctionIntersection, "IntersectionFunctionsWCallStack", false, true]
-                //, [TypeLattices.FunctionTypeLatticeKinds.FunctionPointwiseLub, "SingleFunctions", true, false]
+                [TypeLattices.FunctionTypeLatticeKinds.FunctionIntersection, "IntersectionFunctionsWCallStack", false, true]
+                , [TypeLattices.FunctionTypeLatticeKinds.FunctionPointwiseLub, "SingleFunctions", true, false]
             ];
 
             var allVariableFlowInsensitivities = [
-                //false
+                false
                 , true
             ]; // TODO add inflationary
             allTypes.forEach((types:[()=>ValueTypeConfig, string])=> {
@@ -222,7 +225,7 @@ describe("Type check traces and display table", function () {
         });
     });
 
-    it("Display table & charts", function (done) {
+    it.only("Display table & charts", function (done) {
         // TODO refactor some of this to separate file
         this.timeout(5 * 60 * 1000);
         PersistentResults.load(PersistentResults.ExperimentResultKinds.TypeChecksResult, (results:AnnotatedExperimentResults<TypeChecksResult>[])=> {
